5b8c2e0c04d6e5288b73a0a659a4ca3756cada75
Law Yang <lawtouayang@gmail.com>
2018-06-13 10:58:26 -0500

    GET call for heroes view working

--- server/public/scripts/controllers/hero.controller.js
+++ server/public/scripts/controllers/hero.controller.js
@@ -1,7 +1,16 @@
 app.controller('HeroesController', function( HeroService ){
     let self = this
-    
+    self.displayArray = [];
+    self.test = 'test';
+
+    self.getHero = function() {
+        HeroService.getHeroes()
+            .then( function(){
+                self.displayArray = HeroService.heroArray;
+                console.log(self.displayArray);
+            })
+    }
 
 
-
+    self.getHero();
 });
--- server/public/scripts/services/heroes.service.js
+++ server/public/scripts/services/heroes.service.js
@@ -1,5 +1,18 @@
 app.service('HeroService', function ($http) {
+    let vm = this;
 
+    // get all heroes to display
+    vm.getHeroes = function(){
+        return $http({
+            method: 'GET',
+            url: '/power'
+        }).then( function( response ){
+            console.log('Success GET call in getHeroes:', response.data);
+            vm.heroArray = response.data
+        }).catch( function(error){
+            console.log('Error GET call in getHeroes:', error)
+        })
+    }
 
 
 
